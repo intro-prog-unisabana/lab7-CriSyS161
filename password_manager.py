751176c0c6670acb590a989b0cd8cb969eb66fe4
Cristian Andrés Sánchez Suárez <cristiansasu@unisabana.edu.co>
2026-04-17 17:28:20 -0500

    V3

--- password_manager.py
+++ password_manager.py
@@ -4,20 +4,23 @@ from caesar import caesar_encrypt
 
 
 def encrypt_single_pass(filename: str) -> None:
-    """TODO: Parte 1."""
-    pass
     with open(filename, "r") as myfile:
         original_password = myfile.read()
     encrypt_password = caesar_encrypt(original_password)
-    with open(myfile, 'w') as filemod:
+    with open(filename, 'w') as filemod:
         filemod.write(encrypt_password)
+
 def encrypt_passwords_in_file(filename: str) -> None:
-    """TODO: Parte 2."""
-    pass
     with open(filename, mode='r') as thefile:
         reader = csv.reader(thefile)
-        for row in reader:
-            print(row)
+        rows = [row for row in reader if row]
+    for i in range(1, len(rows)):
+        original_password = rows[i][2]
+        rows[i][2] = caesar_encrypt(original_password)
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(rows)
+        
 
 
 
